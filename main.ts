7f2cc74fc71c4b6d59eab0c54df54a62af6bbe4b
umamarajpoot <164630754+umamarajpoot@users.noreply.github.com>
2024-04-23 22:10:04 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+
+//Declear a constant `answer` and assign it to the resultof inquirer.prompt function
+
+const answers: {
+    Sentence: string
+} = await  inquirer.prompt([
+{
+    name: "Sentence",
+    type:"input",
+    message: "Enter your sentence to count the word: "
+}
+]
+)
+const words = answers.Sentence.trim().split(" ")
+
+//Print the arrey of words to the console
+console.log(words)
+
+//Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
+
